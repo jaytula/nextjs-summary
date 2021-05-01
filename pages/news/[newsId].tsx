a4cba21d81356d0ddd39b86f6216708a62a2d01b
Jay T <jaytula@gmail.com>
2021-05-01 12:04:04 -0700

    244. Extracting Dynamic Route Data

--- pages/news/[newsId].tsx
+++ pages/news/[newsId].tsx
@@ -1,5 +1,13 @@
+import { useRouter } from "next/router";
+
 const DetailPage = () => {
-  return <h1>The Detail Page</h1>;
+  const router = useRouter();
+  const { newsId } = router.query;
+
+  // send a request to the backend APIO
+  // to fetch the news item with newsId
+
+  return <h1>The Detail Page: {newsId}</h1>;
 };
 
 export default DetailPage;
