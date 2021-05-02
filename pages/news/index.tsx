6ecd7a69cdbf8a370acefdacc8ffdfd781a77173
Jay T <jaytula@gmail.com>
2021-05-02 07:47:45 -0700

    245. Linking Between Pages

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+import { Fragment } from "react";
 const NewsPage = () => {
-  return <h1>The News Page</h1>
-}
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is a Great Framework
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
+};
 
 export default NewsPage;
